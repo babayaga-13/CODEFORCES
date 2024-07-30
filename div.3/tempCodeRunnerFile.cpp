75da32324fda0d86d6ef2f4e989b050d963b7f4c
babayaga-13 <muniruddinmahir@gmail.com>
2024-07-30 20:23:33 +0600

    30/7/24

--- div.3/tempCodeRunnerFile.cpp
+++ div.3/tempCodeRunnerFile.cpp
@@ -1,3 +1,15 @@
- ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+while (h < n)
+        // {
+        //     if (s[h] == 'W')
+        //         c++;
+        //     cout << c << " ";
+        //     if (h - l + 1 == k)
+        //     {
+        //         ans = min(ans, c);
+        //         if (s[l] == 'W')
+        //             c--;
+        //         l++;
+        //     }
+        //     h++;
+        // }
+        // cout << ans << endl;
